8576020cdd6cb26719fc118e70339d5cf1180eef
MNO AI/DT추진그룹 한민지 <minji.han@sk.com>
2021-02-02 18:36:55 +0900

    rank

--- src/components/Rank.js
+++ src/components/Rank.js
@@ -1,6 +1,11 @@
 import '../assets/css/Rank.css'
+import { useState } from 'react';
 
 function Rank() {
+
+  const month = ['전체', '3개월', '6개월', '1년'];
+  const [focus, setFocus] = useState(month[0]);
+
   return (
     <div className="rank-top">
       <div className="content">
@@ -11,10 +16,18 @@ function Rank() {
         </div>
         <div className="location-right">
           <select>
-            <option value="">시·도 선택</option>
+            <option value="">시·군·구 선택</option>
           </select>
         </div>
-      </div>        
+        <div className="month">
+          {month.map((mnth, index) => (
+            <button className={"month-button" + (focus === mnth ? "-active" : "")}
+              onClick={() => setFocus(mnth)} key={index}>
+              {mnth}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
