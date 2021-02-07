c2c5589f8a569d9bc78be73ab1f6cfb95e45d699
MNO AI/DT추진그룹 한민지 <minji.han@sk.com>
2021-02-07 23:57:39 +0900

    rank.js

--- src/js/rank.js
+++ src/js/rank.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const URL = 'https://us-central1-myungdang-f73cd.cloudfunctions.net'
-const URL = 'http://localhost:5001/myungdang-f73cd/us-central1'
+const URL = 'https://us-central1-myungdang-f73cd.cloudfunctions.net'
+// const URL = 'http://localhost:5001/myungdang-f73cd/us-central1'
 
 export function getRank(city, town, index) {
   return axios.get(URL + '/rank?CITY=' + city + "&TOWN=" + town + "&LAST_INDEX=" + index).then(result => {
